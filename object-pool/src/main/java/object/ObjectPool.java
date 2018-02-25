44bf5b2548aa113f6fc0c740164f40d3b9951744
Sourindra Chatterji <sourindra.chatterji@im2175-x1.corp.inmobi.com>
2018-02-25 19:37:51 +0530

    Spring Async cleanup

---
+++ object-pool/src/main/java/object/ObjectPool.java
@@ -0,0 +1,10 @@
+package object;
+
+/**
+ * Created by sourindra.chatterji on 27/01/18.
+ */
+public class ObjectPool {
+}
+
+
+
--- spring-async/src/main/java/async/AppRunner.java
+++ spring-async/src/main/java/async/AppRunner.java
@@ -1,5 +1,6 @@
 package async;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -11,13 +12,13 @@ import java.util.concurrent.CompletableFuture;
  * Created by sourindra.chatterji on 26/01/18.
  */
 @Component
-public class AppRunner implements CommandLineRunner{
+public class AppRunner implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
 
     private final GitHubLookupService gitHubLookupService;
 
-    public AppRunner(GitHubLookupService gitHubLookupService){
+    public AppRunner(GitHubLookupService gitHubLookupService) {
         this.gitHubLookupService = gitHubLookupService;
     }
 
@@ -27,17 +28,22 @@ public class AppRunner implements CommandLineRunner{
         long start = System.currentTimeMillis();
 
         // Kick of multiple, asynchronous lookups
-        CompletableFuture<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
-        CompletableFuture<User> page2 = gitHubLookupService.findUser("CloudFoundry");
-        CompletableFuture<User> page3 = gitHubLookupService.findUser("Spring-Projects");
+        CompletableFuture<User>[] completableFutures = new CompletableFuture[100];
+        RateLimiter rateLimiter = RateLimiter.create(10);
+        for (int i = 0; i < 100; i++) {
+            rateLimiter.acquire();
+            long start1 = System.currentTimeMillis();
+            CompletableFuture<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
+            logger.info("page1 time: " + (System.currentTimeMillis() - start1));
 
+            completableFutures[i] = page1;
+            logger.info("count: {}", i);
+        }
         // Wait until they are all done
-        CompletableFuture.allOf(page1,page2,page3).join();
+        CompletableFuture.allOf(completableFutures).join();
 
         // Print results, including elapsed time
         logger.info("Elapsed time: " + (System.currentTimeMillis() - start));
-        logger.info("--> " + page1.get());
-        logger.info("--> " + page2.get());
-        logger.info("--> " + page3.get());
+
     }
 }
